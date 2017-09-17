3df71bc896feab66b446f83252cad350ad947349
michitaro <michitaro.nike@gmail.com>
2017-09-18 08:17:26 +0900

    improve samples

--- example/src/sample5.vue
+++ example/src/sample5.vue
@@ -26,12 +26,6 @@
 import { StyleFactory } from '../../src'
 import Vue from 'vue'
 
-const baseColor = {
-    backgroundColor: '#365c68',
-    color: 'white',
-    boxShadow: '0 4pt 4pt rgba(0, 0, 0, 0.25)'
-}
-
 const active = {
     backgroundColor: '#436f7c'
 }
@@ -41,14 +35,22 @@ const disabled = {
 }
 
 const separator = {
-    backgroundColor: '#f00'
+    backgroundColor: 'rgba(255, 0, 0, 0.5)'
 }
 
 export default <any>{
     components: {
         'my-theme': StyleFactory({
-            menu: baseColor,
-            menubar: baseColor,
+            menu: {
+                background: 'linear-gradient(to bottom, rgba(0, 0, 0, 0.25), #436f7c)',
+                color: 'white',
+                boxShadow: '0 2pt 4pt rgba(0, 0, 0, 0.5)'
+            },
+            menubar: {
+                background: 'linear-gradient(to bottom, rgba(0, 0, 0, 0.25), #436f7c)',
+                color: 'white',
+                boxShadow: '0 4pt 4pt rgba(0, 0, 0, 0.25)'
+            },
             active,
             disabled,
             separator
