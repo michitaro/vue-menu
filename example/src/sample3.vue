f21fe6e543c5baed26c2f5bc81ed717743882ba0
michitaro <michitaro.nike@gmail.com>
2018-08-29 21:36:35 +0900

    tiny tweak about nested context menu

--- example/src/sample3.vue
+++ example/src/sample3.vue
@@ -16,19 +16,19 @@
         <hsc-menu-context-menu style="margin: 50px;">
             <div class="box" style="padding: 1em;">
                 outer click here(nested case)
-                <hsc-menu-context-menu style="margin: 50px;">
-                  <div class="box" style="padding: 1em;">
-                      inner click here
-                  </div>
-                  <template slot="contextmenu">
-                      <hsc-menu-item label="MenuItem 6" />
-                      <hsc-menu-item label="MenuItem 7" />
-                      <hsc-menu-item label="MenuItem 8">
-                          <hsc-menu-item label="MenuItem 9" />
-                          <hsc-menu-item label="MenuItem 10" />
-                      </hsc-menu-item>
-                  </template>
-              </hsc-menu-context-menu>
+                <hsc-menu-context-menu style="margin: 50px;" @contextmenu.native.stop>
+                    <div class="box" style="padding: 1em;">
+                        inner click here
+                    </div>
+                    <template slot="contextmenu">
+                        <hsc-menu-item label="MenuItem 6" />
+                        <hsc-menu-item label="MenuItem 7" />
+                        <hsc-menu-item label="MenuItem 8">
+                            <hsc-menu-item label="MenuItem 9" />
+                            <hsc-menu-item label="MenuItem 10" />
+                        </hsc-menu-item>
+                    </template>
+                </hsc-menu-context-menu>
             </div>
             <template slot="contextmenu">
                 <hsc-menu-item label="MenuItem 1" />
--- src/contextmenu.vue
+++ src/contextmenu.vue
@@ -1,5 +1,5 @@
 <template>
-    <div @contextmenu.prevent.stop="openMenu" style="display: inline-block;">
+    <div @contextmenu="openMenu" style="display: inline-block;">
         <slot/>
         <x-menu ref="menu">
             <slot name="contextmenu" />
