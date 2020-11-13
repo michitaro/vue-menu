941ccccd3eff582fc6f5a4d7fc33aeb999e2631e
liuzw <liuzw@huatingsoft.com>
2020-11-13 13:30:55 +0800

    add sample9: Programmatic open Context Menu

---
+++ example/src/sample9.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <hsc-menu-style-white>
+            <hsc-menu-context-menu refs="contextmenu">
+                <div style="display: none"></div>
+                <template slot="contextmenu">
+                    <hsc-menu-item label="MenuItem 1" />
+                    <hsc-menu-item label="MenuItem 2" />
+                    <hsc-menu-item label="MenuItem 2">
+                        <hsc-menu-item label="MenuItem 4" />
+                        <hsc-menu-item label="MenuItem 5" />
+                    </hsc-menu-item>
+                </template>
+            </hsc-menu-context-menu>
+        </hsc-menu-style-white>
+        <button @click="click">Click Here</button>
+    </div>
+</template>
+
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    methods: {
+        click(e) {
+            const contextmenu = this.$refs.contextmenu.$refs.menu
+            contextmenu.open()
+            contextmenu.setPosition(e.pageX, e.pageY, 'right')
+        },
+    }
+})
+</script>
